3e963f97888e1d99c1f5f6925cd3fb7bceddf3ad
JanaCorsellas <jana.corsellas@estudiantat.upc.edu>
2024-06-11 10:42:04 +0200

    versió 10

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -12,11 +12,19 @@ import javax.persistence.Query;
 
 public class Application extends Controller {
 
+    //Funcions pels formularis
+
     public static void index() {
         renderTemplate("Application/Agricultius.html");
     }
 
     public static void Inici(){
+        List<Comarca> c = Comarca.findAll();
+        renderArgs.put("comarques", c);
+        renderTemplate("Application/Inici.html");
+    }
+
+    public void Convidat(){
         List<Comarca> c = Comarca.findAll();
         renderArgs.put("comarques", c);
         renderTemplate("Application/Inici.html");
@@ -41,6 +49,23 @@ public class Application extends Controller {
     public void Registre(){
         renderTemplate("Application/Registre.html");
     }
+
+    public static void LoginFormulari (){
+        renderTemplate("Application/LoginFormulari.html");
+    }
+
+    public static void showComarquesPage() {
+        List<Comarca> comarques = Comarca.findAll();
+        if (comarques.isEmpty()) {
+            renderText("No comarques found");
+        } else {
+            renderTemplate("/Application/Inici.html");
+        }
+    }
+
+    //MÈTODES
+
+    //Mètode per donar d'alta/registrar un agricultor
     public static void RegistrarAgricultor(String nom, String cognom, int edat, String usuari, String contrasenya, String comarca){
         Agricultor a = Agricultor.find("byNomAndCognomAndEdatAndUsuariAndContrasenya", nom, cognom, edat, usuari, contrasenya).first();
         if (a == null){
@@ -58,10 +83,28 @@ public class Application extends Controller {
         }
     }
 
-    public static void LoginFormulari (){
-        renderTemplate("Application/LoginFormulari.html");
+    //mètode per donar de baixa/eliminar un agricultor
+    public static void eliminarAgricultor() {
+        String usuari = session.get("usuari");
+        if (usuari == null) {
+            flash.error("Error: No has iniciat sessió.");
+            Application.LoginFormulari();
+            return;
+        }
+
+        Agricultor agricultor = Agricultor.find("byUsuari", usuari).first();
+        if (agricultor != null) {
+            agricultor.delete();
+            session.clear(); // Limpiar la sesión si el usuario actual se ha eliminado
+            //flash.success("L'agricultor ha estat eliminat correctament.");
+            Application.Agricultius(); // Redirigir a la pantalla inicial
+        } else {
+            flash.error("Error: L'agricultor no s'ha trobat.");
+            Application.Principal(); // Redirigir a la página principal del usuario
+        }
     }
 
+    //Mètode per iniciar sessió
     public static void Login (String usuari, String contrasenya){
         Agricultor a = Agricultor.find("byUsuariAndContrasenya", usuari, contrasenya).first();
         if (a == null){
@@ -78,17 +121,13 @@ public class Application extends Controller {
         }
     }
 
+    //Mètode per tancar sessió
     public static void logout() {
         session.clear(); // Limpiar la sesión
         Application.Agricultius(); // Redirigir a la página de inicio
     }
 
-    public void Convidat(){
-        List<Comarca> c = Comarca.findAll();
-        renderArgs.put("comarques", c);
-        renderTemplate("Application/Inici.html");
-    }
-
+    //Mètode per editar les dades del perfil
     public static void editProfile() {
         String usuari = session.get("usuari");
         if (usuari == null) {
@@ -98,6 +137,8 @@ public class Application extends Controller {
         List<Comarca> comarques = Comarca.findAll();
         render(comarques);
     }
+
+    //Mètode per actualitzar el perfil un cop editat
     public static void updateProfile(String nom, String cognom, int edat, String usuari, Long comarcaId) {
         String currentUsuari = session.get("usuari");
         if (currentUsuari == null) {
@@ -133,6 +174,7 @@ public class Application extends Controller {
         }
     }
 
+    //Mètode perquè un agricultor pugui ocupar camps a la seva comarca
     public static void ocuparCamps(String ncomarca, int ncamps) {
         Comarca comarca = Comarca.find("byNcomarca", ncomarca).first();
         if (comarca != null) {
@@ -158,6 +200,7 @@ public class Application extends Controller {
         }
     }
 
+    //Mètode per obtenir els detalls d'una comarca
     public static void getComarcaDetails(String ncomarca) {
         Comarca comarca = Comarca.find("byNcomarca", ncomarca).first();
         if (comarca != null) {
@@ -168,6 +211,16 @@ public class Application extends Controller {
         } else {
             notFound("Comarca not found");
         }
+    }
+
+    //Mètode per calcular l'ocupació de camps d'una comarca
+    public static long calcularOcupacioCamps(Comarca comarca) {
+        List<Agricultor> agricultors = Agricultor.find("byNcomarca", comarca).fetch();
+        Long ocupacioCamps = 0L;
+        for(Agricultor a: agricultors) {
+            ocupacioCamps = ocupacioCamps + a.ncamps;
+        }
+        return ocupacioCamps != null ? ocupacioCamps : 0;
     }
 
     public static class ComarcaDetails {
@@ -184,14 +237,10 @@ public class Application extends Controller {
         }
     }
 
-    public static long calcularOcupacioCamps(Comarca comarca) {
-        List<Agricultor> agricultors = Agricultor.find("byNcomarca", comarca).fetch();
-        Long ocupacioCamps = 0L;
-        for(Agricultor a: agricultors) {
-            ocupacioCamps = ocupacioCamps + a.ncamps;
-        }
-        return ocupacioCamps != null ? ocupacioCamps : 0;
-    }
+
+
+
+    //Mètodes extres, sense ús
 
     public static void AssignaAgricultorComarca(String nom, String cognom, int edat, String ncomarca){
         Comarca c = Comarca.find("byNcomarca", ncomarca).first();
@@ -210,17 +259,7 @@ public class Application extends Controller {
         renderText("S'ha completat correctament");
     }
 
-    public static void showComarquesPage() {
-        List<Comarca> comarques = Comarca.findAll();
-        if (comarques.isEmpty()) {
-            renderText("No comarques found");
-        } else {
-            renderTemplate("/Application/Inici.html");
-        }
-    }
-
     //Servei que llista les comarques que tenen mínim x Agricultors
-    //funciona però hem hagut de posar la variable ncomarca pública
     public static void NomComarquesXAgricultors(int nombreMinAgricultors){
         List<Comarca> lc = Comarca.findAll();
         List<String> result = new ArrayList<String>();
@@ -237,7 +276,6 @@ public class Application extends Controller {
     }
 
     //Servei que llista tots els agricultors de la base de dades
-    //hem hagut de posar nom en public
     public static void NomAgricultors(){
         List<Agricultor> la = Agricultor.findAll();
         List<String> result2 = new ArrayList<String>();
